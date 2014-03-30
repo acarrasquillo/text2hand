fee571de4285f240ade765774da3f6f571daf57c
acarrasquillo <abimael.carrasquillo@gmail.com>
2014-03-30 03:17:37 -0400

    removed error

--- app.py
+++ app.py
@@ -1,27 +1,24 @@
 from flask import Flask, url_for, render_template, jsonify, request
 from pymongo import MongoClient
 from sendgrid import SendGridClient, Mail
-from flaskext.coffee2js import coffee2js
-from settings import *
+# from settings import *
 
 app = Flask(__name__)
 app.debug = True
 
-sg = SendGridClient(sg_user, sg_pass)
+# sg = SendGridClient(sg_user, sg_pass)
 
-def sendmail(email,subject, html):
-	try:
-		message = Mail()
-		message.set_from('text2hand@hackpr.com')
-		message.add_to(email)
-		message.set_subject(subject)
-		message.set_html(html)
-		status, msg = sg.send(message)
-		return True
-	except:
-		return False
-
-coffee2js(app, js_folder='js', coffee_folder='src/coffee')
+# def sendmail(email,subject, html):
+# 	try:
+# 		message = Mail()
+# 		message.set_from('text2hand@hackpr.com')
+# 		message.add_to(email)
+# 		message.set_subject(subject)
+# 		message.set_html(html)
+# 		status, msg = sg.send(message)
+# 		return True
+# 	except:
+# 		return False
 
 @app.route('/', methods = ['GET','POST'])
 def hello():
