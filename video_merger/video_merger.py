df0bcd712752a203f0b7cd13792e9a7f7ea579f6
acarrasquillo <abimael.carrasquillo@gmail.com>
2014-03-30 03:14:05 -0400

    video maker added unique videoname

--- video_merger/video_merger.py
+++ video_merger/video_merger.py
@@ -1,14 +1,15 @@
-import subprocess
+import time
 import os
 
 file_list=["../videos/tomorrow.mp4",'../videos/i.mp4','../videos/eat.mp4','../videos/pizza.mp4']
 
+def makeVideo(file_list):
 
-f=open("list.tmp","w")
-for i in file_list:
-	f.write("file '"+ i +"'\n")
-f.close()
-
-os.system("ffmpeg -f concat -i list.tmp -c copy output.mp4")
-os.remove('list.tmp')
-
+	f=open("list.tmp","w")
+	for i in file_list:
+		f.write("file '"+ i +"'\n")
+	f.close()
+	name = str(time.time())
+	os.system("ffmpeg -f concat -i list.tmp -c copy %s.mp4" % name)
+	os.remove('list.tmp')
+	return name +'.mp4'
